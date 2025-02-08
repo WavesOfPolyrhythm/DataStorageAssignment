3c27f13e55f1c90ad04554848eed5c271c1b77c3
Tim <tim..newton.larsson@outlook.com>
2025-02-08 19:57:14 +0100

    Implemented presentation layer with Dependency Injection for testing CRUD operations. Added eager loading to fetch data from repositories.

--- Data/Repositories/ProjectRepository.cs
+++ Data/Repositories/ProjectRepository.cs
@@ -1,10 +1,21 @@
 ﻿using Data.Contexts;
 using Data.Entities;
 using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data.Repositories;
 
 public class ProjectRepository(DataContext context) : BaseRepository<ProjectEntity>(context), IProjectRepository
 {
     private readonly DataContext _context = context;
+
+    public override async Task<IEnumerable<ProjectEntity>> GetAllAsync()
+    {
+        return await _context.Projects
+            .Include(p => p.Status)
+            .Include(p => p.Customer)
+            .ThenInclude(c => c.CustomerContacts)
+            .Include(p => p.Employee) 
+            .ToListAsync();
+    }
 }
---
+++ Presentation/Program.cs
@@ -0,0 +1,32 @@
+﻿using Data.Contexts;
+using Data.Interfaces;
+using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceCollection = new ServiceCollection();
+
+serviceCollection.AddDbContext<DataContext>(options => options.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Projects\DataStorage_Assignment\Data\Databases\database_assignment.mdf;Integrated Security=True;Connect Timeout=30"));
+
+
+serviceCollection.AddScoped<IProjectRepository, ProjectRepository>();
+serviceCollection.AddScoped<ICustomerRepository, CustomerRepository>();
+serviceCollection.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var projectRepository = serviceProvider.GetRequiredService<IProjectRepository>();
+
+var projects = await projectRepository.GetAllAsync();
+
+Console.WriteLine("==== Projects ====");
+foreach (var project in projects)
+{
+    Console.WriteLine($"ID: P-{project.Id}");
+    Console.WriteLine($"Title: {project.Title}");
+    Console.WriteLine($"Status: {project.Status?.StatusName}");
+    Console.WriteLine($"Customer: {project.Customer?.CustomerName}");
+    Console.WriteLine($"Manager: {project.Employee?.Name} ({project.Employee?.Email})");
+}
+
+Console.ReadLine();
